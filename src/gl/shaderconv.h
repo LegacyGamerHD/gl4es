18722113e129175ace3964641bebbf85350de762
WesleyVanNeck <wesleyvanneck1012@gmail.com>
2024-10-02 11:38:19 +0200

    Update shaderconv.h

--- src/gl/shaderconv.h
+++ src/gl/shaderconv.h
@@ -4,8 +4,7 @@
 #include "gles.h"
 #include "program.h"
 
-char * ConvertShaderConditionally(struct shader_s * shader_source);
-char* ConvertShader(const char* pBuffer, int isVertex, shaderconv_need_t *need, int forwardPort);
+char* ConvertShader(const char* pBuffer, int isVertex, shaderconv_need_t *need);
 
 int isBuiltinAttrib(const char* name);
 int isBuiltinMatrix(const char* name);
