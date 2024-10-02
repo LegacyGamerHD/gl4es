3bdd6067a122751b3ad1cfd0e94f23493d9bd3a8
WesleyVanNeck <wesleyvanneck1012@gmail.com>
2024-10-02 11:54:04 +0200

    Update shaderconv.c

--- src/gl/shaderconv.c
+++ src/gl/shaderconv.c
@@ -1262,11 +1262,11 @@ char* ConvertShader(const char* pEntry, int isVertex, shaderconv_need_t *need)
   }
   
   if (versionHeader > 1) {
-//    const char* GLESBackport = "#define texture2D texture\n#define attribute in\n#define varying out\n";
-  //  Tmp = gl4es_inplace_insert(gl4es_getline(Tmp, 1), GLESBackport, Tmp, &tmpsize);
-//  }else {
+////  const char* GLESBackport = "#define texture2D texture\n#define attribute in\n#define varying out\n";
+ ////   Tmp = gl4es_inplace_insert(gl4es_getline(Tmp, 1), GLESBackport, Tmp, &tmpsize);
+////  }else {
   const char* GLESForwardPort = "#define texture texture2D\n #define textureProj texture2DProj\n #define mod(a,b) (int(a) - int(b) * int(a/b))\n";
-    Tmp = gl4es_inplace_insert(gl4es_getline(Tmp, 1), GLESBackport, Tmp, &tmpsize);
+    Tmp = gl4es_inplace_insert(gl4es_getline(Tmp, 1), Tmp, &tmpsize);
   }
 
   // finish
