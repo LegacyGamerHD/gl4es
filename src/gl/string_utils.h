a996bbeffd6198115f7dd892e40caab1910f8d6b
WesleyVanNeck <wesleyvanneck1012@gmail.com>
2024-10-02 11:23:51 +0200

    Update string_utils.h

--- src/gl/string_utils.h
+++ src/gl/string_utils.h
@@ -3,22 +3,17 @@
 
 extern const char* AllSeparators;
 
-int isDigit(char value);
-int isValidFunctionName(char value);
-
-const char* FindString(const char* pBuffer, const char* S);
-char* FindStringNC(char* pBuffer, const char* S);
-int CountString(const char* pBuffer, const char* S);
-char* ResizeIfNeeded(char* pBuffer, int *size, int addsize);
-char* InplaceReplace(char* pBuffer, int* size, const char* S, const char* D);
-char * InplaceReplaceByIndex(char* pBuffer, int* size, int startIndex, int endIndex, const char* replacement);
-char * InplaceInsertByIndex(char * source, int *sourceLength, int insertPoint, const char *insertedString);
-char* Append(char* pBuffer, int* size, const char* S);
-char* InplaceInsert(char* pBuffer, const char* S, char* master, int* size);
-char* GetLine(char* pBuffer, int num);
-int CountLine(const char* pBuffer);
-int GetLineFor(const char* pBuffer, const char* S); // get the line number for 1st occurent of S in pBuffer
-char* StrNext(char *pBuffer, const char* S); // mostly as strstr, but go after the substring if found
+const char* gl4es_find_string(const char* pBuffer, const char* S);
+char* gl4es_find_string_nc(char* pBuffer, const char* S);
+int gl4es_count_string(const char* pBuffer, const char* S);
+char* gl4es_resize_if_needed(char* pBuffer, int *size, int addsize);
+char* gl4es_inplace_replace(char* pBuffer, int* size, const char* S, const char* D);
+char* gl4es_append(char* pBuffer, int* size, const char* S);
+char* gl4es_inplace_insert(char* pBuffer, const char* S, char* master, int* size);
+char* gl4es_getline(char* pBuffer, int num);
+int gl4es_countline(const char* pBuffer);
+int gl4es_getline_for(const char* pBuffer, const char* S); // get the line number for 1st occurent of S in pBuffer
+char* gl4es_str_next(char *pBuffer, const char* S); // mostly as strstr, but go after the substring if found
 //"blank" (space, tab, cr, lf,":", ",", ";", ".", "/")
 char* gl4es_next_str(char* pBuffer);   // go to next non "blank"
 char* gl4es_prev_str(char* Str, char* pBuffer);    // go to previous non "blank"
