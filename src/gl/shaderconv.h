0bf3e79036c8c034d0e205043efd4655ec2f0687
WesleyVanNeck <wesleyvanneck1012@gmail.com>
2024-10-02 11:37:31 +0200

    Update shaderconv.h

--- src/gl/shaderconv.h
+++ src/gl/shaderconv.h
@@ -9,4 +9,8 @@ char* ConvertShader(const char* pBuffer, int isVertex, shaderconv_need_t *need);
 int isBuiltinAttrib(const char* name);
 int isBuiltinMatrix(const char* name);
 
+const char* hasBuiltinAttrib(const char* vertexShader, int Att);
+const char* builtinAttribGLName(const char* name);
+const char* builtinAttribInternalName(const char* name);
+
 #endif // _GL4ES_SHADERCONV_H_
